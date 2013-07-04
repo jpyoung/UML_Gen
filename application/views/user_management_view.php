323b3970ae08dccc97687d5118f7201f7b73f75e
Jack <jackp@vayoungs.us>
2013-07-04 14:50:54 -0400

    added update nav bar
    added update nav bar to the user management page.

--- application/views/user_management_view.php
+++ application/views/user_management_view.php
@@ -16,8 +16,18 @@
 <body>
 
 	<div class="main-nav">
-		<h3 style="margin-left: 30px; color: white; font-size: 21px; font-weight: bold;">UML_Gen</h3>
-	</div>
+		<div style="margin-left: 30px; width: 300px;">
+			<h3 style="color: white; font-size: 21px; font-weight: bold;">UML_Gen</h3>
+		</div>
+		<div style="position:absolute; top: 16px; right: 40px;">
+		    <img src="<? echo base_url(); ?>assets/img/top_right_bigger.png" border="0" usemap="#Map">
+			<p style="position:absolute; left: 47px; top: 1px;">Username: <?php echo $this->session->userdata('username'); ?></p>
+			<map name="Map">
+		      <area shape="rect" coords="10,4,41,35" href="#" alt="user profile link">
+		      <area shape="rect" coords="210,5,240,36" href="<?php echo base_url(); ?>" alt="logout button">
+		    </map>
+		</div>
+	</div>  <!-- end div main-nav -->
 
 <div id="outerWrapper">
 
