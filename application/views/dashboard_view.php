03422031d2820e3742421192a55390e1052a35e0
Jack <jackp@vayoungs.us>
2013-07-05 08:46:12 -0400

    fix some side bar styles

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -143,8 +143,12 @@
 			color: black;
 			padding-left: 15px;
 		}
+		
+		
 	
 	</style>
+
+<link href="<?php echo base_url(); ?>assets/css/sidebarStyle.css" rel='stylesheet' type='text/css'>
 
 </head>
 <body>
@@ -174,10 +178,40 @@
 			
 			<div id="side_buffer_top"></div>
 			
-			<ul>
-				<li><?php echo anchor('welcome/goto_user_management_page', 'User Management'); ?></li>
-				<li><?php echo anchor('welcome/goto_user_preferences', 'User Preferences'); ?></li>
-			</ul>
+			<div class="navi">
+				<ul class="main-side_nav">
+					<li>
+						<a href="dashboard.html" class="light">
+							<div class="ico" style="background-color: white;">
+								<img class="left_bar_image" width="14px" height="14px" src="<?php echo base_url(); ?>assets/img/glyphicons/png/glyphicons_020_home.png">
+								</div>
+							Dashboard
+							<span class="label label-warning">10</span>
+						</a>
+					</li>
+					<li>
+						<a href="forms.html" class="light">
+							<div class="ico" style="background-color: white;">
+								<img class="left_bar_image"width="14px" height="14px" src="<?php echo base_url(); ?>assets/img/glyphicons/png/glyphicons_043_group.png">
+							</div>
+							User Management
+							<span class="label label-info">1</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="<?php echo base_url(); ?>index.php/welcome/goto_user_management_page" class="light">
+							<div class="ico" style="background-color: white;">
+								<img class="left_bar_image"width="14px" height="14px" src="<?php echo base_url(); ?>assets/img/glyphicons/png/glyphicons_029_notes_2.png">
+							</div>
+							User Preferences
+							<span class="label label-info">1</span>
+						</a>
+					</li>
+
+				</ul>
+			</div>  <!-- end div navi -->
+			
 			
 		</div>  <!-- end div sidebar -->
 		
