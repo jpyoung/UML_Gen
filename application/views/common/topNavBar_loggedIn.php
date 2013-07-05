7846c17d49c57fffbc6cb2f973cd4d5a30570087
Jack <jackp@vayoungs.us>
2013-07-05 09:19:49 -0400

    added topNavBar_loggedIn partial view

---
+++ application/views/common/topNavBar_loggedIn.php
@@ -0,0 +1,14 @@
+<div style="margin-left: 30px; width: 300px;">
+	<h3 style="color: white; font-size: 21px; font-weight: bold;">UML_Gen</h3>
+</div>
+
+<div style="position:absolute; top: 16px; right: 40px;">
+
+    <img src="<? echo base_url(); ?>assets/img/top_right_bigger.png" border="0" usemap="#Map">
+
+	<p style="position:absolute; left: 47px; top: 1px;">Username: <?php echo $this->session->userdata('username'); ?></p>
+	<map name="Map">
+      <area shape="rect" coords="10,4,41,35" href="<?php echo base_url();?>index.php/welcome/goto_user_profile_view" alt="user profile link">
+      <area shape="rect" coords="210,5,240,36" href="<?php echo base_url(); ?>" alt="logout button">
+    </map>	
+</div>
--- application/views/user_profile_view.php
+++ application/views/user_profile_view.php
@@ -2,161 +2,22 @@
 <html lang="en">
 <head>
 
-	
 	<meta charset="utf-8">
 	<title>User Profile View</title>
 
 	<link href="<?php echo base_url(); ?>assets/css/style.css" rel='stylesheet' type='text/css'>
 
+	<link href="<?php echo base_url(); ?>assets/css/main_page_layout.css" rel='stylesheet' type='text/css'>
 
-
-	<style type="text/css">
-	
-
-	.containerHeader {
-		/*background: #0567ad;*/
-		background: #0567ad;
-		color: white;
-		font-size: 21px;
-	}
-		
-		/*the below styles are for the table, same style used on the usermanagement page table*/
-		#newspaper-b
-		{
-			font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
-			font-size: 12px;
-			margin: 45px;
-			/*width: 600px;*/
-			text-align: left;
-			border-collapse: collapse;
-			border: 1px solid #69c;
-		}
-		#newspaper-b th
-		{
-			padding: 15px 10px 10px 10px;
-			font-weight: normal;
-			font-size: 14px;
-			color: #039;
-		}
-		#newspaper-b tbody
-		{
-			background: #e8edff;
-		}
-		#newspaper-b td
-		{
-			padding: 10px;
-			color: #669;
-			border-top: 1px dashed #fff;
-		}
-		#newspaper-b tbody tr:hover td
-		{
-			color: #339;
-			background: #d0dafd;
-		}
-	
-		
-		
-		/*fixing page layout and adding side bar*/
-		
-		html,body {
-			min-height: 100%;
-		    
-		}
-		
-		#belowNavWrapper {
-			height: 100%;
-			/*min-height:100%;*/
-		}
-		
-		#wrapper {
-			/*background: #d7d7d7;*/
-		    width: 100%;
-		    height: 100%;
-		}
-		
-		#sidebar {
-			/*background-color: orange;*/
-		    float:left;
-		    width:300px;
-			height: 100%;
-		}
-		
-		#content {
-			/*background-color: blue;*/
-			background-color: white;
-			
-			/*40 px from top nav bar to content box and 300 px(same size as sidebar width) from left.*/
-		    margin: 0 0 0 300px;
-			padding-top: 10px;
-			padding-bottom: 100px;
-		}
-		
-		#containerr{
-			background: white;
-			margin-top: 10px;
-			margin-left: 40px;
-			border: 1px solid #D0D0D0;
-			-webkit-box-shadow: 0 0 8px #D0D0D0;
-		}
-		
-		#side_buffer_top {
-			/*background: #d7d7d7;*/
-			height: 30px;
-		}
-		
-		li {
-			line-height: 50px;
-			list-style: none;
-			border: 1px solid black;
-			
-			border-radius: 5px;
-			-webkit-border-radius: 5px;
-			-moz-border-radius: 5px;
-			
-			margin-bottom: 15px;	
-		}
-		
-		ul, ol {
-			padding: 0;
-			margin: 5px 10px 20px 25px;
-		}
-		
-		li:hover {
-			background-color: #0088cc;    
-		}
-		
-		li a {
-			font-size: 15px;
-			text-decoration:none;
-			color: black;
-			padding-left: 15px;
-		}
-		
-		
-	
-	</style>
-
-<link href="<?php echo base_url(); ?>assets/css/sidebarStyle.css" rel='stylesheet' type='text/css'>
+	<link href="<?php echo base_url(); ?>assets/css/sidebarStyle.css" rel='stylesheet' type='text/css'>
 
 </head>
 <body>
 
 
 	<div class="main-nav">
-		<div style="margin-left: 30px; width: 300px;">
-			<h3 style="color: white; font-size: 21px; font-weight: bold;">UML_Gen</h3>
-		</div>
-	
-		<div style="position:absolute; top: 16px; right: 40px;">
-		
-		    <img src="<? echo base_url(); ?>assets/img/top_right_bigger.png" border="0" usemap="#Map">
-
-			<p style="position:absolute; left: 47px; top: 1px;">Username: <?php echo $this->session->userdata('username'); ?></p>
-			<map name="Map">
-		      <area shape="rect" coords="10,4,41,35" href="#" alt="user profile link">
-		      <area shape="rect" coords="210,5,240,36" href="<?php echo base_url(); ?>" alt="logout button">
-		    </map>	
-		</div>
+		<!-- loading the partial topNavBar_loggedIn view -->
+		<?php include('common/topNavBar_loggedIn.php'); ?>
 	</div>  <!-- end div main-nav -->
 
 
