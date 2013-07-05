22b993f9fd9215e1bd45634ed2f7db5787b82f4d
Jack <jackp@vayoungs.us>
2013-07-05 08:51:01 -0400

    fixed links on side bar

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -181,18 +181,18 @@
 			<div class="navi">
 				<ul class="main-side_nav">
 					<li>
-						<a href="dashboard.html" class="light">
+						<a href="<?php echo base_url(); ?>index.php/welcome" class="light">
 							<div class="ico" style="background-color: white;">
-								<img class="left_bar_image" width="14px" height="14px" src="<?php echo base_url(); ?>assets/img/glyphicons/png/glyphicons_020_home.png">
+								<img class="left_bar_image" width="15px" height="15px" src="<?php echo base_url(); ?>assets/img/glyphicons/png/glyphicons_020_home.png">
 								</div>
 							Dashboard
 							<span class="label label-warning">10</span>
 						</a>
 					</li>
 					<li>
-						<a href="forms.html" class="light">
+						<a href="<?php echo base_url(); ?>index.php/welcome/goto_user_management_page" class="light">
 							<div class="ico" style="background-color: white;">
-								<img class="left_bar_image"width="14px" height="14px" src="<?php echo base_url(); ?>assets/img/glyphicons/png/glyphicons_043_group.png">
+								<img class="left_bar_image"width="15px" height="15px" src="<?php echo base_url(); ?>assets/img/glyphicons/png/glyphicons_043_group.png">
 							</div>
 							User Management
 							<span class="label label-info">1</span>
@@ -200,9 +200,9 @@
 					</li>
 
 					<li>
-						<a href="<?php echo base_url(); ?>index.php/welcome/goto_user_management_page" class="light">
+						<a href="<?php echo base_url(); ?>index.php/welcome/goto_user_preferences" class="light">
 							<div class="ico" style="background-color: white;">
-								<img class="left_bar_image"width="14px" height="14px" src="<?php echo base_url(); ?>assets/img/glyphicons/png/glyphicons_029_notes_2.png">
+								<img class="left_bar_image"width="15px" height="15px" src="<?php echo base_url(); ?>assets/img/glyphicons/png/glyphicons_029_notes_2.png">
 							</div>
 							User Preferences
 							<span class="label label-info">1</span>
