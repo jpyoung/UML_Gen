bc90defb270e75fa5a033c7de46a3769e1375eff
Jack <jackp@vayoungs.us>
2013-07-20 17:51:32 -0400

    Pull CHanges

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -17,15 +17,15 @@ class Dashboard extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	
-	
+
+
 	function __construct() {
 		parent::__construct();
 		$this->load->helper('url');
-	
+
 	}
-	
-	
+
+
 	public function index() {
 		if($this->session->userdata('user_type') == 1) {
 			$this->dashboard_page();
@@ -33,46 +33,46 @@ class Dashboard extends CI_Controller {
 			redirect('login');
 		}
 	}
-	
-	
+
+
 	function dashboard_page() {
-		
+
 		$this->auth->check_session();
-		
+
 		//gathering the user preferences
 		$temp_pref = $this->retrieve_user_preferences();
-		
+
 		//Setting the colors that were retrieved
 		$data['background_color'] = $temp_pref['background_color'];
 		$data['panel_background_color'] = $temp_pref['panel_background_color'];
 		$data['container_header_color'] = $temp_pref['container_header_color'];
-		
+
 		//getting all the users information
 		$data['user_info'] = $this->get_all_users();
-		
+
 		//getting all the file information 
 		$data['files_info'] = $this->get_all_files();
-	
+
 		// $this->load->view('welcome_message', $data);
 		$this->load->view('dashboard_view', $data);
 	}
-	
-	
+
+
 	//this function is used to retrieve the user preferences that are
 	//stored in the user_site_pref table.
 	function retrieve_user_preferences() {
-		
+
 		$query_string = 'select * from user_site_pref where user_id =' . $this->session->userdata('user_id');
 
 		$query = $this->db->query($query_string);
-		
+
 		//below are the default pre array colors for the site
 		$pref_array = array(
 			"background_color" => 'white',
 			"panel_background_color" => 'white',
 			"container_header_color" => '#0567ad'
 		);
-		
+
 		if ($query->num_rows() > 0) {
 			//found a row in the user_site_pref table for the given user
 			$ro = $query->row_array();
@@ -87,8 +87,8 @@ class Dashboard extends CI_Controller {
 
 		return $containerHeaderColor;
 	}
-	
-	
+
+
 	//This function is used to update the users preferences.
 	//It is called from the user_preferences_view php page.  
 	function update_user_preferences() {
@@ -102,106 +102,102 @@ class Dashboard extends CI_Controller {
 		$this->db->update("user_site_pref", $data);
 		$this->goto_user_management_page();
 	}
-	
-		
+
+
 	//function is called when an admin would like to add a new user to the db.
 	function create_new_user() {
-		
+
 		$this->load->view('create_user_view');
 	}
-	
+
 	//used to query the db and return all the users. 
 	function get_all_users() {
 		return $this->db->query('select * from user')->result();
 	}
-	
+
 	//used to query the db and return all the file table.
 	function get_all_files() {
 		return $this->db->query('select * from file')->result();
 	}
-	
-	
+
+
 	//this is used to go to the user management view page. 
 	function goto_user_management_page() {
-		
+
 		$data['user_info'] = $this->get_all_users();
 		$this->load->view('user_management_view', $data);
 	}
-	
-	
+
+
 	//used to go to the user preferences view page.
 	function goto_user_preferences() {
 		$data['user_info'] = $this->get_all_users();
 		$this->load->view('user_preferences_view', $data);
 	}
-	
+
 	//used to go ot the detailed_user_view page called from the user_management_view.php page.
 	//accepts the user_id of the selected user
 	function goto_detailed_user_view($id) {
-		
+
 		//grabbing the individual users information
 		$data['user_info'] = $this->get_user_by_id($id);
-		
+
 		//grabbing the all the files associated with this user id
 		$data['user_files'] = $this->get_files_by_user($id);
-		
+
 		$this->load->view('detailed_user_view', $data);
 	}
-	
-	
+
+
 	//used to go to the user_profile view
 	function goto_user_profile_view() {
-		
+
 		//gathering the user information from the DB. 
 		$data['user_info'] = $this->get_user_by_id($this->session->userdata('user_id'));
-		
+
 		$this->load->view('user_profile_view', $data);
 	}
-	
+
 	//used to go the the uml diagrams page
 	function goto_uml_diagrams() {
-<<<<<<< HEAD
-		$this->load->view('uml_diagram_view');
-=======
 		$this->auth->check_session();
-		
+
 		//gathering the user preferences
 		$temp_pref = $this->retrieve_user_preferences();
-		
+
 		//Setting the colors that were retrieved
 		$data['background_color'] = $temp_pref['background_color'];
 		$data['panel_background_color'] = $temp_pref['panel_background_color'];
 		$data['container_header_color'] = $temp_pref['container_header_color'];
-		
+
 		//getting all the users information
 		$data['user_info'] = $this->get_all_users();
-		
+
 		//getting all the file information 
 		$data['files_info'] = $this->get_all_files();
 		$this->load->view('diagrams_view', $data);
-		
+
 	}
-	
-	
+
+
 	//The user clicked the generate uml button in the actions column
 	function goto_detailed_diagrams($select_file_id) {
-		
+
 		if ($select_file_id != null) {
 			$data['select_file_id'] = $select_file_id;
 		} else {
 			//generate_uml_button
 			$data['select_file_id'] = "Nothing";
 		}
-		
-	
-		
+
+
+
 		$this->load->view('detailed_diagrams_view', $data);
-		
->>>>>>> Update
+
 	}
 
-	
-	
+
+
 	//function is used to get user by the passed in id
 	//and return the resulting query
 	function get_user_by_id($id) {
@@ -213,8 +209,8 @@ class Dashboard extends CI_Controller {
 		} 
 		return false;
 	}
-	
-	
+
+
 	//function is used to get all the files uploaded
 	//by the passed in user id. Returns the resulting query. 
 	function get_files_by_user($user_id) {
@@ -246,17 +242,17 @@ class Dashboard extends CI_Controller {
 		$this->db->insert("user", $info);
 		$this->goto_user_management_page();
 	}
-	
+
 	//used to change users password on the user profile view page
 	function change_password() {
-		
+
 		//using the current users id to query update
 		$where_at = $this->session->userdata('user_id');
-		
+
 		$currentPass = $_POST['currentPass'];
 		$newPass = $_POST['newPass'];
 		$confirmPass = $_POST['confirmPass'];
-		
+
 		//echo "currentPass= " . $currentPass . " new pass= " . $newPass . " confirmPass= " . $confirmPass;
 		if ($this->session->userdata('password') == $currentPass) {
 			//The password they logged in with matches the password they entered into to update their password
@@ -274,11 +270,11 @@ class Dashboard extends CI_Controller {
 			echo 'Error, the current password.';
 		}	
 	}
-	
-	
+
+
 	//used to save the profile user information on the user profile view page
 	function save_profile_user_info() {
-		
+
 		$user_id = $this->session->userdata('user_id');
 		$user_info_submitted = array(
 			'u_name' => $_POST['name'], 
@@ -288,19 +284,19 @@ class Dashboard extends CI_Controller {
 			'u_state' => $_POST['state'],
 			'u_zip' => $_POST['zip']
 		);
-		
+
 		//using the user management model to update the table
 		$this->load->model('user_management_model');
 		$rke = $this->user_management_model->edit_user_info($user_info_submitted, $user_id);
 		echo "true";
 	}
-	
+
 	function edit_user_info($profile_data, $whereAt)
 	{
 		 $this->db->where('user_id',$whereAt);
 	     $this->db->update('users', $profile_data);	
 	}
-	
-	
+
+
 
 }
