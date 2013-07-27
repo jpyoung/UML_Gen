ac1e4274b9c9a8b74b6ea824526fc743953ec2c3
Jack <jackp@vayoungs.us>
2013-07-27 10:56:08 -0400

    created test file

--- application/views/detailed_diagrams_view.php
+++ application/views/detailed_diagrams_view.php
@@ -20,12 +20,47 @@
 	
 <h3>UML Generate View</h3>
 
-
-<div style="background-color: blue; width: 400px;">
-	<pre>
-public class diagram {
+<div style="margin-left: 20px; margin-right: 40px; background-color: yellow;">
+	asdf
 	
+	<div style="background-color: green;">
+		
+	</div>
+	
+	<div style="background-color: blue;">
+		
+	</div>
+	
+</div>
+
+<div style="margin-left: 20px; border: 3px solid green; width: 600px;">
+	<pre>
+		<!-- <?php
+		
+		$fta = array();
+		
+		$fh = fopen("/Users/youngbuck14188/Sites/UML_Gen/uploaded_files/outer.java", "r");
+		while(! feof($fh)) {
+			$fta[] = trim(fgets($fh));
+		}
+		fclose($fh);
+		
+		for ( $x = 0; $x < count($fta); $x++ ) {
+			echo $fta[$x] . "<br/>";
+		}
+		
+		 //print_r($fta);
+		?> -->
+
+public class Apple {
+	private String name;
+	private int age;
+	
+	public getAge() {
+		return this.age;
+	}
 }
+
 	</pre>
 </div>
 
@@ -36,7 +71,9 @@ echo "<p> The File ID is: " . $select_file_id . "</p>";
 
 ?>
 
-
+<div style="margin-left: 20px; border: 3px solid blue; width: 400px;">
+    <table border="1"><tr><th>Outer</th></tr><tr><td><p>-name; String</p><p>+address; String</p></td></tr><tr><td><p>&lt;&lt;constructor&gt;&gt;Outer()</p><p>&lt;&lt;constructor&gt;&gt;Outer(name: String, address: String)</p><p style="text-decoration:underline">+getName(): String</p><p style="text-decoration:underline">+setName(name: String): void</p><p>+doMath(min: int, max: int, slope: int): String</p><p style="text-decoration:underline">-getMath(): void</p><p>#setAge(age: int, yearsOld: int): String</p></td></tr></table>  
+</div>
 
 </body>
 </html>
