f5f576decc04e170ebb7ff1132f3d38a9ae6e6ca
Jack <jackp@vayoungs.us>
2013-07-27 14:55:53 -0400

    fixed links in new sidebar

--- application/views/common/sidebar.php
+++ application/views/common/sidebar.php
@@ -3,14 +3,14 @@
 	<div class="navi">
 		<ul class='main-nav'>
 			<li class='active'>
-				<a href="dashboard.html" class='light'>
+				<a href="<?php echo base_url(); ?>index.php/dashboard" class='light'>
 					<div class="ico"><i class="icon-home icon-white"></i></div>
 					Dashboard
 					<span class="label label-warning">10</span>
 				</a>
 			</li>
 			<li>
-				<a href="<?php echo base_url(); ?>index.php/dashboard/goto_user_preferences" class='light'>
+				<a href="<?php echo base_url(); ?>index.php/dashboard/goto_user_management_page" class='light'>
 					<div class="ico"><i class="icon-list icon-white"></i></div>
 					User Management
 					<span class="label label-info">1</span>
@@ -25,13 +25,11 @@
 			</li>
 			
 			<li>
-				<a href="<?php echo base_url(); ?>index.php/dashboard/goto_user_preferences" class='light'>
+				<a href="<?php echo base_url(); ?>index.php/dashboard/goto_uml_diagrams" class='light'>
 					<div class="ico"><i class="icon-th-large icon-white"></i></div>
 					UML Diagrams
 					<span class="label label-info">1</span>
 				</a>
 			</li>
-		
-
 		</ul>
 </div>  
