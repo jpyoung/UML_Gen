4960bed8308cb29a9826efb782ae5b1ebffa9614
Jack <jackp@vayoungs.us>
2013-07-27 13:48:08 -0400

    created footer partial view

---
+++ application/views/common/footer.php
@@ -0,0 +1,16 @@
+</div>  <!-- end content div -->
+
+		
+<script src="<?php echo base_url(); ?>/theme/js/jquery.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/less.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/jquery.peity.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/jquery.fancybox.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/jquery.flot.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/jquery.color.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/jquery.flot.resize.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/jquery.cookie.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/jquery.cookie.js"></script>
+<script src="<?php echo base_url(); ?>/theme/js/custom.js"></script><script src="js/demo.js"></script>
+</body>
+</html>
