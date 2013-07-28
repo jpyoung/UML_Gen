6f32cda1463ca43974c9b058e8d030a2f02d5863
Jack <jackp@vayoungs.us>
2013-07-27 20:26:12 -0400

    Added Active Sidebar Highlighting

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -19,6 +19,48 @@ $(document).ready(function() {
 	
 	//on document load, set the body background to retina-wood.
 	$('body').removeClass().addClass("retina-wood");
+	
+	var t = $('title').html();
+	//console.log("Page Title: " + t);
+	
+	var c = $("ul.main-nav li");
+	
+	//removing the active side bar
+	for (var x = 0; x < c.length; x++) {
+		//console.log("[" + x + "]  = " + $(c[x]).attr("class"));
+		$(c[x]).removeClass();
+	}
+	
+	//logic tree deciding which sidebar element should be highlighted
+	if (t == "Dashboard") {
+		highlight_this(0, c);
+	} else if (t == "Uploader") {
+		highlight_this(1, c);
+	} else if (t == "User Management") {
+		highlight_this(2, c);
+	} else if (t == "User Preferences") {
+		highlight_this(3, c);
+	} else if (t == "UML Diagrams") {
+		highlight_this(4, c);
+	}
+	
+	//used to high the element, c is the array, e is the index
+	function highlight_this(e, c) {
+		$(c[e]).addClass('active');
+	}
+
+	// $("ul.main-nav li").click(function(e){
+	// 	
+	// 	var c = $("ul.main-nav li");
+	// 	//var elements = document.getElementsByTagName('div');
+	// 	for (var x = 0; x < c.length; x++) {
+	// 		console.log("[" + x + "]  = " + $(c[x]).attr("class"));
+	// 		$(c[x]).removeClass();
+	// 	}
+	// 	$(this).addClass('active');
+	// 	//return false;
+	// });
+	
 
 });
 
